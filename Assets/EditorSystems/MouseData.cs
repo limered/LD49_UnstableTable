d713ffbc0bd03c5f3f002e02862ed1be68610d41
limered <me@limered.de>
2022-02-14 23:32:52 +0100

    extract mouse data

---
+++ Assets/EditorSystems/MouseData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace EditorSystems
+{
+    public struct MouseData
+    {
+        /// <summary>
+        /// 2D Mouse Position only over editor render window (Center is (0,0))
+        /// </summary>
+        public Vector2 EditorPosition;
+        
+        /// <summary>
+        /// Mouse Position in World (z-coord is camera coord)
+        /// </summary>
+        public Vector3 MouseWorldPosition;
+        
+        /// <summary>
+        /// Button Numbers: -1 no button, 0 left, 2 right, 3 middle
+        /// </summary>
+        public int MouseButtonClicked;
+    }
+}
