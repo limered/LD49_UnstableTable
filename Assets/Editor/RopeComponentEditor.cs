c4fbb905c26350978ed28f807c457c40540cec46
limered <me@limered.de>
2022-02-14 23:33:47 +0100

    add rope component editor

---
+++ Assets/Editor/RopeComponentEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Systems.RopeSimulation;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(RopeComponent))]
+    public class RopeComponentEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI () {
+            //Called whenever the inspector is drawn for this object.
+            DrawDefaultInspector();
+            
+            var ropeSystem = GameEditor.Systems.GetSystem<RopeSimulationCreationSystem>();
+            var nodeButtonLabel = ropeSystem.Mode == RopeCreationMode.Idle ? 
+                "Add Rope Point" : 
+                "Stop Adding Elements";  
+            if (GUILayout.Button(nodeButtonLabel))
+            {
+                if (ropeSystem.Mode == RopeCreationMode.Idle)
+                {
+                    ropeSystem.CurrentClickedComponent = (RopeComponent) target;
+                    ropeSystem.GoToMode(RopeCreationMode.CreatePoint);
+                }
+                else
+                {
+                    ropeSystem.CurrentClickedComponent = null;
+                    ropeSystem.GoToMode(RopeCreationMode.Idle);
+                }
+            }
+             
+            if (GUILayout.Button("Add Rope Stick")) 
+            {
+            }
+        }
+    }
+}
