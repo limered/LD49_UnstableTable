cbfdd9b5c75436bfc7198a1e268604be5ebd738e
limered <me@limered.de>
2022-02-15 01:56:29 +0100

    cleanup naming in gameeditor

---
+++ Assets/Editor/SystemBase/GameEditor.cs
@@ -0,0 +1,46 @@
+﻿using SystemBaseEditorCommon;
+using UniRx;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.SystemBase
+{
+    [InitializeOnLoad]
+    public class GameEditor
+    {
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static readonly ReactiveProperty<MouseData> MouseData = new ReactiveProperty<MouseData>();
+        public static readonly SystemCollection Systems = new SystemCollection();
+
+        static GameEditor(){
+            Debug.Log("Game Editor started");
+
+            Systems.Initialize();
+            
+            SceneView.duringSceneGui += OnSceneGUI;
+        }
+
+        private static void OnSceneGUI(SceneView view)
+        {
+            UpdateMouseData(view);
+        }
+
+        private static void UpdateMouseData(SceneView sceneView)
+        {
+            
+            if (!Event.current.isMouse) return;
+            
+            var cur = Event.current;
+            var mousePos = (Vector3) cur.mousePosition;
+            mousePos.y = Camera.current.pixelHeight - mousePos.y;
+            var worldPos = sceneView.camera.ScreenToWorldPoint(mousePos);
+            
+            MouseData.SetValueAndForceNotify(new MouseData
+            {
+                editorPosition = mousePos,
+                mouseWorldPosition = worldPos,
+                mouseButtonClicked = cur.clickCount > 0 ? cur.button : -1,
+            });
+        }
+    }
+}
